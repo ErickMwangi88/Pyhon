73c37f16f8b7f577eee2337c189c95475eb83d7f
ErickMwangi88 <123925225+ErickMwangi88@users.noreply.github.com>
2023-02-06 16:35:13 +0300

    Add files via upload

---
+++ logindetails.py
@@ -0,0 +1,9 @@
+#let the username be user
+#let the password be student
+username = input("Enter the username: ")
+password = input("Enter the password: ")
+
+if (username == "user" and password == "student"):
+    print("login successful")
+else:
+    print("Invalid Credentials")
