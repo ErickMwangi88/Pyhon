476eb8556cf76a3e89eb02a15db2aa5c7289319d
ErickMwangi88 <123925225+ErickMwangi88@users.noreply.github.com>
2023-03-02 00:06:30 +0300

    Delete python prac directory

--- python prac/volume.py
+++
@@ -1,8 +0,0 @@
-#volume pi * radius * radius * height
-
-from math import pi
-def volume_sphere(radius,height):
-    volume = pi * radius * radius * height
-    return volume;
-
-print("The volume of the cylinder is: ",(volume_sphere(7,14)))
